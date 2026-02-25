5e91a7e0e30539bc970e75c7f064e201764a574b
Rui Fernandes <ruipedro16@protonmail.com>
2026-02-25 01:32:08 +0000

    plugin autentcacao.gov

---
+++ cc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Instala o plugin Autenticação.Gov (https://autenticacao.gov.pt/fa/ajuda/autenticacaogovpt.aspx#installAgent)
+
+
+
+if [ ! -f plugin-autenticacao-gov.deb ]; then
+    wget -O plugin-autenticacao-gov.deb https://aplicacoes.autenticacao.gov.pt/plugin/plugin-autenticacao-gov.deb
+    sudo apt install -y ./plugin-autenticacao-gov.deb
+else
+    echo "plugin-autenticacao-gov.deb ja existe"
+fi
