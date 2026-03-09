689c59698662da65b6e7707bd3dd4171d7167a3e
Rui Fernandes <ruipedro16@protonmail.com>
2026-03-09 11:58:44 +0000

    z

--- cc.sh
+++ cc.sh
@@ -7,6 +7,7 @@
 if [ ! -f plugin-autenticacao-gov.deb ]; then
     wget -O plugin-autenticacao-gov.deb https://aplicacoes.autenticacao.gov.pt/plugin/plugin-autenticacao-gov.deb
     sudo apt install -y ./plugin-autenticacao-gov.deb
+    rm plugin-autenticacao-gov.deb
 else
     echo "plugin-autenticacao-gov.deb ja existe"
 fi
